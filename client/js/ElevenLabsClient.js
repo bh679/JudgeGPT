18272f563e461afb75e2b4fb5fadadfd8ddd128e
Brennan Hatton <brennan@brennanhatton.com>
2023-08-05 21:06:03 +1000

    Queueing is now working from the elevenlands file

--- client/js/ElevenLabsClient.js
+++ client/js/ElevenLabsClient.js
@@ -1,40 +1,66 @@
 // A class that manages speech synthesis through an external API
 class SpeechManager {
     // The constructor initializes the class with the API domain and sets up some state variables
-    constructor(apiDomain) {
+    constructor(apiDomain, status) {
         this.apiDomain = apiDomain;  // The domain of the API to fetch speech from
         this.currentAudio = null;  // The Audio object of the currently playing speech
         this.queue = [];  // A queue of speech tasks
         this.voicing = true;  // A flag indicating whether speech synthesis is currently allowed
         this.isSpeaking = false;  // A flag indicating whether speech synthesis is currently happening
+        
+        if(status)
+            this.status = stutus;
+    }
+
+    AddToStatus(newLine, reset)
+    {
+        console.log(newLine);
+
+        if(reset)
+            if (this.status != null)
+                this.status.innerText = "";
+        
+        if (this.status != null) this.status.innerText += newLine + "\n";
     }
 
     // The Speak function initiates a speech task
-    async Speak(text, voice, callBack, status) {
+    async Speak(text, voice, callBack) {
         // If the system is not currently allowed to speak, or if there is an audio currently playing, or if a speech task is already happening, return immediately
-        if (!this.voicing || this.currentAudio || this.isSpeaking)
+        if (!this.voicing)
+            return;
+
+        //if new thing to be said
+        if(text != null && voice != null)
+        {
+            console.log("Adding to queue");
+
+            // Add the speech task to the queue
+            this.queue.push({text: text, voice: voice, callBack: callBack});
+
+        }
+
+        if(this.isSpeaking)
             return;
 
         this.isSpeaking = true;  // Set the isSpeaking flag to true
 
-        // Add the speech task to the queue
-        this.queue.push({text: text, voice: voice, callBack: callBack, status: status});
+        console.log("getting message to play");
 
         // Get the next speech task from the queue
         let message = this.queue.shift();
 
         // Update the status text
-        if (status != null) status.innerText = "Speak: " + message.text + '\n';
+        this.AddToStatus("Speak: " + message.text, true);
 
         try {
             // Update the status text
-            if (status != null) status.innerText += 'Processing...\n';
+            this.AddToStatus('Processing...');
 
             // Fetch the speech audio from the API
             const response = await this.fetchSpeech(message.text, message.voice);
 
             // Update the status text
-            if (status != null) status.innerText += 'Speech successfully generated!\n';
+            this.AddToStatus('Speech successfully generated!');
 
             // Create a blob URL from the response
             const blob = await response.blob();
@@ -57,7 +83,7 @@ class SpeechManager {
         } catch (error) {
             // Log the error and update the status text
             console.error('Error:', error);
-            if (status != null) status.innerText += 'Error: ' + error.message + '\n';
+            this.AddToStatus('Error: ' + error.message);
         }
     }
 
@@ -82,7 +108,7 @@ class SpeechManager {
     handleAudioEnd(audio, callBack, status) {
         audio.onended = () => {
             // Update the status text
-            if (status != null) status.innerText += 'Audio has finished playing!\n';
+            this.AddToStatus('Audio has finished playing!');
 
             // Call the callback function if it's provided
             if (callBack != null)
@@ -90,12 +116,12 @@ class SpeechManager {
 
             this.currentAudio = null;  // Clear the currently playing audio
 
+            this.isSpeaking = false; // Set the isSpeaking flag to false
+
             // If there are more speech tasks in the queue, start the next one
             if (this.queue.length > 0) {
                 this.Speak();
-            } else {
-                this.isSpeaking = false;  // Set the isSpeaking flag to false
-            }
+            } 
         };
     }
 
